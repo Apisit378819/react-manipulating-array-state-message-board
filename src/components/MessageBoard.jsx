68810e689e9b9750519f6a45c10c54a764bf57ba
Tonkla <Apisit378819@gmail.com>
2024-10-24 14:30:29 +0700

    add text

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
 function MessageBoard() {
+  const [message, setMessage] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      id: message.length + 1,
+      message: inputMessage,
+    };
+    setMessage([...message, newMessage]);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <form class="message-input-container" onSubmit={addMessage}>
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={(event) => setInputMessage(event.target.value)}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
+        <button className="submit-message-button" type="submit">
+          Submit
+        </button>
+      </form>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+          {message.map((message) => (
+            <div key={message.id} className="message">
+              <div>{message.message}</div>
+              <button className="delete-button">x</button>
+            </div>
+          ))}        
       </div>
     </div>
   );
